3693d8bde9abc951b98d762a0733c137717bda89
Petr Filatov <pmfilatov@gmail.com>
2020-10-19 20:03:00 +0300

    Added posterProject

--- src/main/java/ru/netology/manager/PosterManager.java
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -3,34 +3,33 @@ package ru.netology.manager;
 import ru.netology.domain.PosterItem;
 
 public class PosterManager {
-  private PosterItem[] items = new PosterItem[0];
-  private int amountOfPosters = 10;
+    private PosterItem[] items = new PosterItem[0];
+    private int limitOfPosters = 10;
 
-  public void add(PosterItem item) {
-    // создаём новый массив размером на единицу больше
-    int length = items.length + 1;
-    PosterItem[] tmp = new PosterItem[length];
-    // itar + tab
-    // копируем поэлементно
-    // for (int i = 0; i < items.length; i++) {
-    //   tmp[i] = items[i];
-    // }
-    System.arraycopy(items, 0, tmp, 0, items.length);
-    // кладём последним наш элемент
-    int lastIndex = tmp.length - 1;
-    tmp[lastIndex] = item;
-    items = tmp;
-  }
 
-  public PosterItem[] getAll() {
-    PosterItem[] result = new PosterItem[items.length];
-    // перебираем массив в прямом порядке
-    // но кладём в результаты в обратном
-    for (int i = 0; i < amountOfPosters - 1; i++) {
-      int index = items.length - i - 1;
-      result[i] = items[index];
+    public void add(PosterItem item) {
+        int length = items.length + 1;
+        PosterItem[] tmp = new PosterItem[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
     }
-    return result;
-  }
 
+    public PosterItem[] getAll() {
+        PosterItem[] result = new PosterItem[items.length];
+        PosterItem[] limitOfPosters = new PosterItem[this.limitOfPosters];
+        for (int i = 0; i < items.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        if (items.length >= this.limitOfPosters) {
+            for (int i = 0; i < this.limitOfPosters; i++) {
+                limitOfPosters[i] = result[i];
+            }
+            return limitOfPosters;
+        } else {
+            return result;
+        }
+    }
 }
--- src/test/java/ru/netology/manager/PosterManagerTest.java
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -8,36 +8,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PosterManagerTest {
     private PosterManager posterManager = new PosterManager();
-    PosterItem first = new PosterItem(1,1,"first","genre");
-    PosterItem second = new PosterItem(1,1,"second","genre");
-    PosterItem third = new PosterItem(1,1,"third","genre");
-    PosterItem fourth = new PosterItem(1,1,"fourth","genre");
-    PosterItem fifth = new PosterItem(1,1,"fifth","genre");
-    PosterItem sixth = new PosterItem(1,1,"sixth","genre");
-    PosterItem seventh = new PosterItem(1,1,"seventh","genre");
-    PosterItem eighth = new PosterItem(1,1,"eighth","genre");
-    PosterItem nineth = new PosterItem(1,1,"nineth","genre");
-    PosterItem tenth = new PosterItem(1,1,"tenth","genre");
-    PosterItem eleventh = new PosterItem(1,1,"eleventh","genre");
+    PosterItem first = new PosterItem(1, 1, "first", "genre");
+    PosterItem second = new PosterItem(1, 1, "second", "genre");
+    PosterItem third = new PosterItem(1, 1, "third", "genre");
+    PosterItem fourth = new PosterItem(1, 1, "fourth", "genre");
+    PosterItem fifth = new PosterItem(1, 1, "fifth", "genre");
+    PosterItem sixth = new PosterItem(1, 1, "sixth", "genre");
+    PosterItem seventh = new PosterItem(1, 1, "seventh", "genre");
+    PosterItem eighth = new PosterItem(1, 1, "eighth", "genre");
+    PosterItem nineth = new PosterItem(1, 1, "nineth", "genre");
+    PosterItem tenth = new PosterItem(1, 1, "tenth", "genre");
+    PosterItem eleventh = new PosterItem(1, 1, "eleventh", "genre");
 
-
-//    @BeforeEach
-//    void SetUp() {
-//        posterManager.add(first);
-//        posterManager.add(second);
-//        posterManager.add(third);
-//        posterManager.add(fourth);
-//        posterManager.add(fifth);
-//        posterManager.add(sixth);
-//        posterManager.add(seventh);
-//        posterManager.add(eighth);
-//        posterManager.add(nineth);
-//        posterManager.add(tenth);
-//        posterManager.add(eleventh);
-//    }
 
     @Test
     void GetAllLessLimit() {
+        posterManager.add(first);
+        posterManager.add(second);
+        posterManager.add(third);
+        PosterItem[] actual = posterManager.getAll();
+        PosterItem[] expected = new PosterItem[]{third, second, first};
+
+        assertArrayEquals(actual, expected);
+
+    }
+
+    @Test
+    void getAllEqualsLimit() {
+        posterManager.add(first);
+        posterManager.add(second);
+        posterManager.add(third);
+        posterManager.add(fourth);
+        posterManager.add(fifth);
+        posterManager.add(sixth);
+        posterManager.add(seventh);
+        posterManager.add(eighth);
+        posterManager.add(nineth);
+        posterManager.add(tenth);
+        PosterItem[] actual = posterManager.getAll();
+        PosterItem[] expected = new PosterItem[]{tenth, nineth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+        assertArrayEquals(actual, expected);
 
     }
 
@@ -55,7 +65,7 @@ class PosterManagerTest {
         posterManager.add(tenth);
         posterManager.add(eleventh);
         PosterItem[] actual = posterManager.getAll();
-        PosterItem[] expected = new PosterItem[]{eleventh, tenth, nineth, eighth, seventh, sixth, fifth, fourth,third, second};
+        PosterItem[] expected = new PosterItem[]{eleventh, tenth, nineth, eighth, seventh, sixth, fifth, fourth, third, second};
         assertArrayEquals(actual, expected);
     }
 
